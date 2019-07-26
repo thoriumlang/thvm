cdf38ae593c008c84c6fbef963a3f2e7c8cf5b48
Christophe Pollet <cpollet@users.noreply.github.com>
2019-07-27 00:52:55 +0200

    Fix sonar analysis

--- docker/build.sh
+++ docker/build.sh
@@ -2,15 +2,13 @@
 
 env
 
-mkdir /build
 cd /build || exit
 
-echo Build /sources in "$(pwd)..."
-cmake /sources
-
-make clean all test
+echo Build /src in "$(pwd)..."
+cmake -DCMAKE_BUILD_TYPE=Debug /src
 
 if [ "$SONAR" = "true" ]; then
-  echo "Sonar is enabled"
-  /build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir /bw-output make clean thvm
+  /build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-output make clean all test
+else
+  make clean all test
 fi
